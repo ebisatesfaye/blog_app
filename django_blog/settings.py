c219a2fd4468d04597eb42ffaff0b376d4084e71
ebisa <ebisatesfaye30@gmail.com>
2025-03-31 08:17:56 +0300

    static file

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
 
-STATIC_URL = 'static/'
+STATIC_URL = 'blog/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
