56faf26b3770ed2e825c07b42afe9200c8d48da1
ebisa <ebisatesfaye30@gmail.com>
2025-03-31 20:00:29 +0300

    last

--- env_vars.py
+++ env_vars.py
@@ -9,10 +9,17 @@ db_password = os.environ.get('USER_PASS')
 
 print(db_user)
 print(db_password)
-# "dest": "django_blog/wsgi.py"
+
 db_url = os.getenv('DATABASE_URL')
 print(db_url)
 print('   \n')
 database_url = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 print(database_url)
 
+# "dest": "django_blog/wsgi.py"
+
+    #  "build": {
+    #        "env": {
+    #          "PG_CONFIG": "/usr/local/bin/pg_config"
+    #        }
+    #     } 
